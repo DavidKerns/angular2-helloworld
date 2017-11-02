df2fb45b4900ecf1928fca813c93cd8db3c667f8
DavidKerns <davidkerns@yahoo.com>
2017-11-01 20:01:19 -0400

    cool dog added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-}
+  export class AppComponent {
+      title:   string = 'Welcome to Angular';
+      content: string = 'A new world is waiting for you!';
+      image: string = 'http://i0.kym-cdn.com/entries/icons/original/000/002/203/20533_cooldog.jpg';
+  }
